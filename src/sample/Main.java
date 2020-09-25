e1ce297205188670387d33a31f1cd2dd489947df
ahmedbashir-repo <ahmedbashir.w@gmail.com>
2020-09-25 11:57:18 +0500

    Slider and ComboBox

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,6 +9,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -26,6 +27,12 @@ public class Main extends Application {
         SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1,100,1);
         spinner.setValueFactory(valueFactory);
 
+        Slider hSlider = new Slider(0,100,0);
+        Slider vSlider = new Slider(0,100,0);
+        vSlider.setOrientation(Orientation.VERTICAL);
+
+        ComboBox<String> countriesComboBox = new ComboBox<String>();
+        countriesComboBox.getItems().addAll("Pakistan","Iran","India","KSA");
         ProgressBar progressBar = new ProgressBar(0);
 //        progressBar.setProgress(0.5);
         spinner.valueProperty().addListener(new ChangeListener<Integer>() {
@@ -38,10 +45,22 @@ public class Main extends Application {
                     completedCb.setSelected(false);
                 }
                 progressBar.setProgress(1.00*newVal/100);
+                hSlider.setValue(newVal);
+                vSlider.setValue(newVal);
             }
         });
 
-        FlowPane fp = new FlowPane(heightLb, widthLb, tf,tf2,spinner,completedCb,progressBar);
+        hSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldVal, Number newVal) {
+                vSlider.setValue(newVal.doubleValue());
+            }
+        });
+
+        FlowPane fp = new FlowPane(heightLb, widthLb, tf,tf2,spinner,completedCb,progressBar,hSlider,vSlider,countriesComboBox);
+        fp.setHgap(25);
+        fp.setVgap(25);
+
         tf.focusedProperty().addListener(new ChangeListener<Boolean>() {
             @Override
             public void changed(ObservableValue<? extends Boolean> observableValue, Boolean oldVal, Boolean newVal) {
@@ -53,26 +72,42 @@ public class Main extends Application {
                 }
             }
         });
+        tf.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldVal, String newVal) {
+                if(newVal.length()>10){
+//                    Alert.AlertType alertAlertType;
+//                    Alert alert = new Alert(AlertType.INFORMATION);
+//                    alert.setTitle("Length Exceeded");
+//                    alert.setContentText("Length cannot be greater than 10");
+//                    alert.showAndWait();
+                    tf2.setDisable(true);
+                }
+                else{
+                    tf2.setDisable(false);
+                }
+            }
+        });
         widthLb.setFont(new Font(32));
         heightLb.setFont(new Font(32));
         fp.setOrientation(Orientation.VERTICAL);
         fp.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(fp,250,250);
-//        scene.heightProperty().addListener(new ChangeListener<Number>() {
-//            @Override
-//            public void changed(ObservableValue<? extends Number> observableValue, Number oldVal, Number newVal) {
-//                heightLb.setText("Height: " + newVal);
-//            }
-//        });
-//
-//        scene.widthProperty().addListener(new ChangeListener<Number>() {
-//            @Override
-//            public void changed(ObservableValue<? extends Number> observableValue, Number oldVal, Number newVal) {
-//                widthLb.setText("Width: " + oldVal);
-//            }
-//        });
-//
+        scene.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldVal, Number newVal) {
+                heightLb.setText("Height: " + newVal);
+            }
+        });
+
+        scene.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldVal, Number newVal) {
+                widthLb.setText("Width: " + oldVal);
+            }
+        });
+
         primaryStage.setScene(scene);
         primaryStage.setTitle("Properties and Bindings Demo");
         primaryStage.show();
