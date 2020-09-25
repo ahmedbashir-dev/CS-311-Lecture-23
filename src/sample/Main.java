3a00d668598e3ea39cc3ee14344cd1c531a85e1e
ahmedbashir-repo <ahmedbashir.w@gmail.com>
2020-09-25 11:36:01 +0500

    Change Listener for Spinner is added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,8 +8,7 @@ import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -22,7 +21,24 @@ public class Main extends Application {
         Label widthLb = new Label();
         TextField tf = new TextField();
         TextField tf2 = new TextField();
-        FlowPane fp = new FlowPane(heightLb, widthLb, tf,tf2);
+        CheckBox completedCb = new CheckBox("Completed");
+        Spinner<Integer> spinner = new Spinner<Integer>();
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1,100,1);
+        spinner.setValueFactory(valueFactory);
+
+        spinner.valueProperty().addListener(new ChangeListener<Integer>() {
+            @Override
+            public void changed(ObservableValue<? extends Integer> observableValue, Integer oldVal, Integer newVal) {
+                if(newVal == 100){
+                    completedCb.setSelected(true);
+                }
+                else{
+                    completedCb.setSelected(false);
+                }
+            }
+        });
+
+        FlowPane fp = new FlowPane(heightLb, widthLb, tf,tf2,spinner,completedCb);
         tf.focusedProperty().addListener(new ChangeListener<Boolean>() {
             @Override
             public void changed(ObservableValue<? extends Boolean> observableValue, Boolean oldVal, Boolean newVal) {
@@ -40,19 +56,20 @@ public class Main extends Application {
         fp.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(fp,250,250);
-        scene.heightProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number oldVal, Number newVal) {
-                heightLb.setText("Height: " + newVal);
-            }
-        });
-
-        scene.widthProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number oldVal, Number newVal) {
-                widthLb.setText("Width: " + oldVal);
-            }
-        });
+//        scene.heightProperty().addListener(new ChangeListener<Number>() {
+//            @Override
+//            public void changed(ObservableValue<? extends Number> observableValue, Number oldVal, Number newVal) {
+//                heightLb.setText("Height: " + newVal);
+//            }
+//        });
+//
+//        scene.widthProperty().addListener(new ChangeListener<Number>() {
+//            @Override
+//            public void changed(ObservableValue<? extends Number> observableValue, Number oldVal, Number newVal) {
+//                widthLb.setText("Width: " + oldVal);
+//            }
+//        });
+//
         primaryStage.setScene(scene);
         primaryStage.setTitle("Properties and Bindings Demo");
         primaryStage.show();
